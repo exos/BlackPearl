e8bc774ea335786de29f23af5f572cb0f2b6c42e
exos <tioscar@gmail.com>
2012-10-24 16:52:54 -0300

    Mmm raro, pero estaba mal el autoload

--- autoload.php
+++ autoload.php
@@ -6,7 +6,12 @@ function blackpearl_classes_autoload ($class) {
     $classPath = explode('\\',$class);
 
     if ($classPath[0] == 'BlackPearl') {
+
+        array_shift($classPath);
+
         $file = dirname(__FILE__) . "/" . strtolower(implode('/',$classPath)) .".php";
+
+        echo "busco $file";
 
         if (file_exists($file)) {
             require_once($file);
